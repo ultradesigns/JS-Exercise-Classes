6b957918c191b59abdf802a01b80292e338fd3e1
Lance Sanchez <lancesanchez@outlook.com>
2020-11-12 20:28:51 -0500

    Task 1

--- index.js
+++ index.js
@@ -41,9 +41,23 @@ class Airplane {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-class Person {
-
+class Person{
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(food){
+    if(this.stomach.length < 10){
+      this.stomach.push(food);
+    } else{
+      this.stomach = [];
+    }
+    return toString(`${this.name}, is ${this.age} yrs. old.`);
+  }
 }
+
+
 
 /*
   TASK 2
